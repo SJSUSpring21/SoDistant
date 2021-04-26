b03425b784f8169c272f832462beba410e0ce03b
srpanchal <srpanchal2712@gmail.com>
2021-04-26 00:27:32 -0700

    yolov4

--- code/config.py
+++ code/config.py
@@ -9,10 +9,10 @@ MAIL = ''
 
 URL = 0
 MIN_DISTANCE = 50
-CV_INP_WIDTH = 320
-CV_INP_HEIGHT = 320
-WEIGHTS = "yolov3.weights"
-CFG = "yolov3.cfg"
-# WEIGHTS = "yolov3-tiny.weights"
-# CFG = "yolov3-tiny.cfg"
+CV_INP_WIDTH = 416
+CV_INP_HEIGHT = 416
+# WEIGHTS = "yolov3.weights"
+# CFG = "yolov3.cfg"
+WEIGHTS = "yolov4-tiny.weights"
+CFG = "yolov4-tiny.cfg"
 VIDEO_PATH = "videos/"
