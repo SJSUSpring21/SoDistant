1beb6f436e659e11edd3361a89c3e219e2c56db5
srpanchal <srpanchal2712@gmail.com>
2021-05-09 01:43:46 -0700

    mail

--- code/mail.py
+++ code/mail.py
@@ -1,19 +1,15 @@
 import smtplib, ssl
 
 
-class Mailer:
-
-    def __init__(self):
-        self.EMAIL = "abcd@gmail.com"
-        self.PASS = "abcd"
-        self.PORT = 465
-        self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
-
-    def send(self, mail):
-        self.server = smtplib.SMTP_SSL('smtp.gmail.com', self.PORT)
-        self.server.login(self.EMAIL, self.PASS)
-        SUBJECT = 'ALERT!'
-        TEXT = f'Social distancing violations exceeded!'
-        message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-        self.server.sendmail(self.EMAIL, mail, message)
-        self.server.quit()
+def send(mail):
+    EMAIL = "sodistantapp@gmail.com"
+    PASS = "sodistant12"
+    PORT = 465
+    server = smtplib.SMTP_SSL('smtp.gmail.com', PORT)
+    server = smtplib.SMTP_SSL('smtp.gmail.com', PORT)
+    server.login(EMAIL, PASS)
+    SUBJECT = 'ALERT!'
+    TEXT = f'Social distancing violations exceeded!'
+    message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+    server.sendmail(EMAIL, mail, message)
+    server.quit()
